6e31e3fd6b1086c48ecb3240c6b4fb36e608a15f
emjayko <m.juranyi@gmail.com>
2013-04-11 21:36:57 +0200

    Major update

--- src/literate/init.lua
+++ src/literate/init.lua
@@ -3,61 +3,106 @@
 -- @release 2013/03/18, Michal Juranyi
 -------------------------------------------------------------------------------
 
-local io, table, ipairs, pairs, type, print = io, table, ipairs, pairs, type, print 
+--local io, table, ipairs, pairs, type, print, pcall = io, table, ipairs, pairs, type, print, pcall
 
 local string = require "string"
+local re = require "re"
+local scanner = require "leg.scanner"
+local parser = require "leg.parser"
 local highlighter = require "luapretty.highlighter"
+local util = require "literate.util"
+local lxsh = require "lxsh"
+local luacomments = require "comments"
+require "markdown"
 
-module ("literate")
+module("literate", package.seeall)
 
-local comment_blocks = {}
-local code_tree = { key = 1 }
+--[[# 
+*doc\_blocks* table contains split source file into documentation and code part.
+The table has following structure:
 
-local function containsComment(subtree)
-    local res
-    for _,v in ipairs(subtree) do
-        if v['key'] == "COMMENT_BLOCK" or v['key'] == "COMMENT" then
-            return true
-        elseif #v > 0 then
-            res = containsComment(v)
-            if res == true then
-                return true
+*   doc\_blocks['doc'].str
+*   doc\_blocks['doc'].type
+*   doc\_blocks['code']
+--]]
+local doc_blocks = {}
+local functions = {}
+
+------------------------------------------------------------------------
+-- This function prepares table with separated documentation and code parts,
+-- that will be used to generate HTML output
+-- @name extractCodeNodes
+-- @param ast root node of AST
+local function extractCodeNodes(ast)
+    --[[
+    This function splits AST into LP documentation part and code part.
+    Code part may also contain comments, such as API documentation or commented out code.
+    --]]
+    for i,v in ipairs(ast) do
+        --^ `filter comments`
+        if v.key == "COMMENT" and v.parsed.Literate then
+            if v.parsed.Literate[1].type ~= "lp" then
+                table.insert(doc_blocks, { doc = "", code = ""})
             end
+            if v.parsed.Literate[1].type == "lp" or v.parsed.Literate[1].type == "markdown" then
+                doc_blocks[#doc_blocks].doc = { str = v.parsed.Literate[1].text, type = v.parsed.Literate[1].type }
+            end
+        elseif v.key == "COMMENT" and v.parsed.Custom then
+            if v.parsed.Custom.type == "startblock" then
+                table.insert(doc_blocks, { doc = { str = "<strong>"..v.parsed.Custom.block.."</strong><br/>"..v.parsed.Custom.text, type = v.parsed.Custom.type }, code = ""})
+            elseif v.parsed.Custom.type == "endblock" then
+                table.insert(doc_blocks, { doc = { str = "end of <strong>"..(v.parsed.Custom.block or "").."</strong> block", type = v.parsed.Custom.type }, code = ""})
+            end
+        --v
+        elseif type(v) == "table" and #v > 0 then
+            if (v['key'] == "GlobalFunction" or v['key'] == "LocalFunction") and v.docstring then
+                table.insert(doc_blocks, { doc = "", code = ""})
+            end
+            extractCodeNodes(v)
+        else
+            doc_blocks[#doc_blocks].code = doc_blocks[#doc_blocks].code .. v['str']
         end
     end
-    return false
 end
 
-local comment_block_counter = 0
+local block_comments_stack = {}
+local block_comments_count = 0
 
-local function extractCodeNodes(ast)
-    for _,v in ipairs(ast) do
-        if v['key'] == "COMMENT_BLOCK" then
-            comment_block_counter = comment_block_counter + 1
-        --elseif v['css_extra'] ~= nil or #v == 0 then
-        elseif containsComment(v) == false then
-            print(comment_block_counter)
-            if type(comment_blocks[comment_block_counter].code) ~= "table" then
-                comment_blocks[comment_block_counter].code = { key = 1 }
-            end
-            table.insert(comment_blocks[comment_block_counter].code,v)
-        elseif type(v) == "table" then
-            extractCodeNodes(v)
-        end
+local function block_comments_class()
+    if #block_comments_stack == 0 then
+        return ""
     end
+    --^ `chaining class` This creates class string for HTML begining with "block_comment" and adds block# for each block comment level above current
+    local class = "block_comment"
+    for _,v in ipairs(block_comments_stack) do
+        class = class .. " block"..tostring(v)
+    end
+    --v `chaining class`
+    return class
 end
 
 local function ASTtoHTML(ast)
     local html
+    local class
     extractCodeNodes(ast)
-    dumpTree(comment_blocks)
+    --dumpTree(ast)
+    dumpTree(doc_blocks)
     html = "<table>"
-    for _,v in ipairs(comment_blocks) do
-        html = html .. "<tr><td>"
-        html = html .. v['str']
-        html = html .. "</td><td>"
+    for _,v in ipairs(doc_blocks) do
+        if v.doc.type == "startblock" then
+            block_comments_count = block_comments_count + 1
+            table.insert(block_comments_stack, block_comments_count)
+        elseif v.doc.type == "endblock" then
+            table.remove(block_comments_stack)
+        elseif v.doc.type == "markdown" then
+            v.doc.str = markdown(v.doc.str)
+        end
+        class = block_comments_class()
+        html = html .. '<tr class="'.. class ..'"><td class="docs">'
+        html = html .. (v.doc.str or "")
+        html = html .. '</td><td class="code">'
         html = html .. "<pre class=\"highlighted_code\">"
-        html = html .. highlighter.assemble_table(v.code) 
+        html = html .. lxsh.highlighters.lua(v.code, { formatter = lxsh.formatters.html, external = true })
         html = html .. "</pre>"
         html = html .. "</td></tr>"
     end
@@ -65,107 +110,30 @@ local function ASTtoHTML(ast)
     return html
 end
 
-------------------------------------------------------------------------
--- Function that decide whether subtree begining with current node
--- is comment block
--- @name isCommentBranch
--- @param subtree - root node of tested subtree
--- returns true if subtree contains only COMMENT and SPACE leaves
-local function isCommentBranch(subtree)
-    if #subtree == 0 then
-        if subtree.key == "COMMENT" then 
-            return true
-        elseif subtree.key == "SPACE" then
-            return nil
-        else
-            return false
-        end
-    else
-        local status = nil --testing endline comment
-        local res
-
-        for _,v in ipairs(subtree) do
-            if type(v) == "table" then
-                res = isCommentBranch(v)
-                if type(res) == "boolean" and res == false then
-                    return false
-                elseif type(res) == "nil" then
-                    if type(status) == nil then
-                        status = nil
-                    end
-                elseif type(res) == "boolean" and res == true then
-                    status = true
-                end
-            end
-        end
-        return status
-    end
-end
-
-local function findLastComment(subtree)
-    for i=#subtree,1,-1 do
-        if subtree[i].key == "COMMENT" then
-            return i
-        end
-    end
-end
+local search_docstring = false
+local inline = ""
 
 local last_stat = nil
-
+local search_funcname = false
 ------------------------------------------------------------------------
--- Function extending current AST with LP-related information
--- @name extendTree
+-- Function looking for comments and function definitions
+-- @name findCommentsAndFunctions
 -- @param ast - AST to extend
-local function extendTree(ast)
-    local res
+local function findFunctions(ast)
+    local res, func_node
 
     for i,v in ipairs(ast) do
-        --^ Test each found IGNORED node for contents of COMMENT nodes.
-        -- Other non-leaf nodes will be tested for being Stat nodes,
-        -- in which case they might be related to preceding COMMENT_BLOCK.
-        -- For all other non-IGNORED nodes, algorithm will recurse into them.
-        if v.key == "IGNORED" then
-            res = isCommentBranch(v)
-            --^ If IGNORED node contains COMMENT nodes, move whole IGNORED branch
-            -- into new COMMENT_BLOCK node, which takes the original IGNORED node's place
-            if res then
-                local node = {}
-                node['key'] = "COMMENT_BLOCK"
-                node['str'] = ""
-                for ii,vv in ipairs(v) do
-                    if node[1] == nil then
-                        if vv['key'] == "COMMENT" then
-                            table.insert(node, vv)
-                            v[ii] = nil
-                        end
-                    else
-                        table.insert(node, vv)
-                        v[ii] = nil
-                    end
-                end
-                table.insert(v,node)
-                for i=findLastComment(node)+1,#node do
-                    table.insert(v,node[i])
-                    node[i] = nil
-                end
-                for _,v in ipairs(node) do
-                    node['str'] = node['str'] .. v['str']
-                end
-                table.insert(comment_blocks, node)
-            end
-            --v
-        elseif type(v) == "table" then
-            if v.key == "Stat" then
-                if #comment_blocks > 0 and comment_blocks[#comment_blocks].doc_rel == nil then
-                    v.doc_rel = #comment_blocks
-                    comment_blocks[#comment_blocks].doc_rel = #comment_blocks
-                else
-                    last_stat = v
-                end
-            end
-            extendTree(v)
+        if v.key == "GlobalFunction" or v.key == "LocalFunction" then
+            search_funcname = true
+            func_node = v
+        elseif v.key == "Name" and search_funcname == true then
+            functions[v.str] = { node = func_node }
+            search_funcname = false
         end
-        --v
+
+        if #v > 0 then
+            findFunctions(v)
+        end
     end
 end
 
@@ -175,12 +143,15 @@ end
 -- @name processText
 -- @param code - string containing the source code to be analyzed
 function literate(ast)
-    extendTree(ast)
+    doc_blocks = {}
+    findFunctions(ast)
+    table.insert(doc_blocks, { doc = "", code = "" })
     return ASTtoHTML(ast)
 end
 
 function dumpTree(ast, depth)
-        --print("Node size: "..#ast)
+    --print("Node size: "..#ast)
+    if ast == nil then return nil end
 	if depth==nil then
             depth = 0
         end
@@ -189,13 +160,14 @@ function dumpTree(ast, depth)
         for i=1,depth do
             indent = indent .. "--"
         end
-
         for k,v in pairs(ast) do
             if type(v)~="table" then
                 print(indent..k.." = "..v)
             else
                 print(indent..k.." = ")
-                dumpTree(v,depth+1)
+                if k ~= "comment" then
+                    dumpTree(v,depth+1)
+                end
             end
         end
 end
